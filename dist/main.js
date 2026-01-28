720906ca5a4f6cd43085aaa27d90eddd2a045ead
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-01-28 10:24:34 +0530

    Implemented: Generating a new block.

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const crypto_js_1 = __importDefault(require("crypto-js"));
-const calculateHash = (index, previousHash, timeStamp, data) => crypto_js_1.default.SHA256(index + previousHash + timeStamp + data).toString();
-console.log(calculateHash(123, "asdgas34re", "150273", "data"));
-//# sourceMappingURL=main.js.map
