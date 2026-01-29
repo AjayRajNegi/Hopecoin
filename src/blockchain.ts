d5f73ffb15570e279eebb9e6445e8633ee24a518
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-01-29 09:20:23 +0530

    Implemented: Validating a new block.

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -33,13 +33,13 @@ const genesisBlock: Block = new Block(
 let blockchain: Block[] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockchain;
-
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const generateNextBlock = (blockData: string) => {
   const previousBlock: Block = getLatestBlock();
   const nextIndex: number = previousBlock.index + 1;
   const nextTimeStamp: number = new Date().getTime() / 1000;
+
   const nextHash: string = calculateHash(
     nextIndex,
     previousBlock.hash,
@@ -53,7 +53,14 @@ const generateNextBlock = (blockData: string) => {
     nextTimeStamp,
     blockData,
   );
+
+  addBlock(newBlock);
+  //broadcastLatest()
+  return newBlock;
 };
+
+const calculateHashForBlock = (block: Block): string =>
+  calculateHash(block.index, block.previousHash, block.timeStamp, block.data);
 
 const calculateHash = (
   index: number,
@@ -62,5 +69,44 @@ const calculateHash = (
   data: string,
 ): string =>
   CryptoJS.SHA256(index + previousHash + timeStamp + data).toString();
+
+const addBlock = (block: Block) => {
+  if (isValidNewBlock(block, getLatestBlock())) {
+    blockchain.push(block);
+  }
+};
+
+const isValidBlockStructure = (block: Block): boolean => {
+  return (
+    typeof block.index === "number" &&
+    typeof block.hash === "string" &&
+    typeof block.previousHash === "string" &&
+    typeof block.timeStamp === "number" &&
+    typeof block.data === "string"
+  );
+};
+
+const isValidNewBlock = (newBlock: Block, previousBlock: Block): boolean => {
+  if (!isValidBlockStructure(newBlock)) {
+    console.log("Is invalid newBlock structure.");
+    return false;
+  }
+  if (newBlock.index !== previousBlock.index + 1) {
+    console.log("Is invalid newBlock.");
+    return false;
+  } else if (newBlock.previousHash !== previousBlock.hash) {
+    console.log("Is invalid newBlock.");
+    return false;
+  } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+    console.log(
+      typeof newBlock.hash + " " + typeof calculateHashForBlock(newBlock),
+    );
+    console.log(
+      "invalid hash: " + calculateHashForBlock(newBlock) + " " + newBlock.hash,
+    );
+    return false;
+  }
+  return true;
+};
 
 export default calculateHash;
